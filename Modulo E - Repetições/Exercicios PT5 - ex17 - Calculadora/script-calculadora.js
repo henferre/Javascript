bc2730bff8ba9d9ac57057c316e66977902a364f
Henrique Ferreira <henriquethis@gmail.com>
2020-04-07 14:37:22 -0300

    finalizando a calculadora
    Terminei de construir o scrpit, dei umas pequenas editadas no estilo e arrumei umas coisas q n tinha notado q esqueci no html

--- Modulo E - Repetições/Exercicios PT5 - ex17 - Calculadora/script-calculadora.js
+++ Modulo E - Repetições/Exercicios PT5 - ex17 - Calculadora/script-calculadora.js
@@ -1,3 +1,18 @@
 function tabuada() {
-    
+    let numero = document.getElementById("numero")
+    let tabuada = document.getElementById("tabuada")
+    if (numero.value.length == "") {
+        window.alert("Por favor, digite um número!")
+    } else {
+        let n = Number(numero.value)
+        let c = 0
+        tabuada.innerHTML =""
+        while (c <= 10) {
+            let item = document.createElement("option")
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `tabuada${c}`
+            tabuada.appendChild(item)
+            c++
+        }
+    }
 }
